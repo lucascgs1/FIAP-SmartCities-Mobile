65f83194333ca04bd1948e46ed747ed453e423f5
Lucas Coutinho <lucascgs1@hotmail.com>
2021-09-01 23:37:49 -0300

    Add project files.

---
+++ Poke.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Poke.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
