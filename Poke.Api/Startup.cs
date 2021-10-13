523f57c1bcb26457b538550be65ad38993c57664
Lucas Coutinho <lucascgs1@hotmail.com>
2021-10-12 22:29:01 -0300

    ajustes no swagger

--- Poke.Api/Startup.cs
+++ Poke.Api/Startup.cs
@@ -4,7 +4,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Poke.Api.Configure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Poke.Api
 {
@@ -34,7 +38,48 @@ namespace Poke.Api
             services.AddControllers();
 
             // Configurar swagger
-            services.AddSwaggerSetup();
+            services.AddSwaggerGen(c => {
+                                        c.SwaggerDoc("v1",
+                                            new OpenApiInfo
+                                            {
+                                                Title = "Poke",
+                                                Version = "v1",
+                                                Description = "Api criada para o fiap SmartFits",
+                                                Contact = new OpenApiContact
+                                                {
+                                                    Name = "Lucas Coutinho",
+                                                    Url = new Uri("https://github.com/lucascgs1")
+                                                }
+                                            });
+
+                                        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                                        {
+                                            Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                                            Name = "Authorization",
+                                            In = ParameterLocation.Header,
+                                            Type = SecuritySchemeType.ApiKey,
+                                            Scheme = "Bearer"
+                                        });
+
+                                        c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                                        {
+                                                        {
+                                                            new OpenApiSecurityScheme
+                                                            {
+                                                                Reference = new OpenApiReference
+                                                                {
+                                                                    Type = ReferenceType.SecurityScheme,
+                                                                    Id = "Bearer"
+                                                                },
+                                                                Scheme = "oauth2",
+                                                                Name = "Bearer",
+                                                                In = ParameterLocation.Header,
+                                                            },
+                                                            new List<string>()
+                                                        }
+                                        });
+                                        c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+                                    });
 
             //injecao de dependencia
             services.RegisterServices();
@@ -72,7 +117,12 @@ namespace Poke.Api
                 endpoints.MapControllers();
             });
 
-            app.UseSwaggerSetup();
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Poke");
+            });
         }
     }
 }
